6a3f1aa2284f0308e73abe599e60ab6f603300a7
Amonwan <156374482+Amonwn@users.noreply.github.com>
2024-10-21 13:24:10 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ my_secret_key = st.secrets['MyOpenAIKey']
 os.environ["OPENAI_API_KEY"] = my_secret_key
 
 prompt = st.text_input("What can I help you today?", " ")
-number = st.number_input("Insert expected number of token to be used", min_value=1)
+number = st.number_input("Insert expected number of token to be used")
 
 ### Request the answer to the prompt1 (creativity)
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
