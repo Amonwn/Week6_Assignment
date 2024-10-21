2a3cc59de9da0cf86ee1001c3645e6ca0caa739c
Amonwan <156374482+Amonwn@users.noreply.github.com>
2024-10-21 14:12:04 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,39 +15,35 @@ prompt = st.text_input("What can I help you today?", " ")
 number = st.number_input("Insert expected number of token to be used", min_value=1)
 
 ### Request the answer to the prompt1 (creativity)
-client = OpenAI()
-response = client.chat.completions.create(
-  model="gpt-4o-mini",
-  messages=[
-    {"role": "system", "content": "Complete the following prefix"},
-    {"role": "user", "content": prompt}
-  ],
-  #n=10,
-  max_tokens=number,
-    temperature=0.8
-)
+#ref from https://www.kaggle.com/code/amirmotefaker/chatgpt-web-application-using-streamlit/comments
+rom transformers import GPT2LMHeadModel, GPT2Tokenizer
+model_name = "gpt2" 
+model = GPT2LMHeadModel.from_pretrained(model_name)
+tokenizer = GPT2Tokenizer.from_pretrained(model_name)
+
+input_ids = tokenizer.encode(prompt, return_tensors="pt")
+output = model.generate(input_ids, temperature=0.8)
+generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
+
 
 ### Display
 st.write(
-    response.choices[0].message.content
+    generated_text.choices[0].message.content
 ) 
 
 ### Request the answer to the prompt2 (predictable)
-client = OpenAI()
-response = client.chat.completions.create(
-  model="gpt-4o-mini",
-  messages=[
-    {"role": "system", "content": "Complete the following prefix"},
-    {"role": "user", "content": prompt}
-  ],
-  #n=10,
-  max_tokens=number,
-    temperature=0.2
-)
+rom transformers import GPT2LMHeadModel, GPT2Tokenizer
+model_name = "gpt2" 
+model = GPT2LMHeadModel.from_pretrained(model_name)
+tokenizer = GPT2Tokenizer.from_pretrained(model_name)
+
+input_ids = tokenizer.encode(prompt, return_tensors="pt")
+output = model.generate(input_ids, temperature=0.2)
+generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
 
 
 ### Display
 st.write(
-    response.choices[0].message.content
+    generated_text.choices[0].message.content
 ) 
 
