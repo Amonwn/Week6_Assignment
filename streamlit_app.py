21fa1b9c24b3ec99716a7d5e205a11bd7c79628f
Amonwan <156374482+Amonwn@users.noreply.github.com>
2024-10-21 14:12:27 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ number = st.number_input("Insert expected number of token to be used", min_value
 
 ### Request the answer to the prompt1 (creativity)
 #ref from https://www.kaggle.com/code/amirmotefaker/chatgpt-web-application-using-streamlit/comments
-rom transformers import GPT2LMHeadModel, GPT2Tokenizer
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
 model_name = "gpt2" 
 model = GPT2LMHeadModel.from_pretrained(model_name)
 tokenizer = GPT2Tokenizer.from_pretrained(model_name)
@@ -32,7 +32,7 @@ st.write(
 ) 
 
 ### Request the answer to the prompt2 (predictable)
-rom transformers import GPT2LMHeadModel, GPT2Tokenizer
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
 model_name = "gpt2" 
 model = GPT2LMHeadModel.from_pretrained(model_name)
 tokenizer = GPT2Tokenizer.from_pretrained(model_name)
